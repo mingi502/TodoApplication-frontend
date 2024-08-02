3759914b1c7c646f1994280358f473125403ad5d
mingi502 <chlalsrl503@gmail.com>
2024-08-02 07:12:10 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "http://dev.insert.o-r.kr";
+  backendHost = "https://dev.insert.o-r.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
