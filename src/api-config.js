e9cc8ae989d585a64ee8a10c55f3f0b3bf7099e3
mingi502 <chlalsrl503@gmail.com>
2024-08-06 07:17:52 +0000

    ㅁㄴㅇ

--- src/api-config.js
+++ src/api-config.js
@@ -5,8 +5,9 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
+  backendHost = "https://application.o-r.kr";
   // backendHost = "https://dev.insert.o-r.kr";
-  backendHost = "TodoApplication00-backend-env.eba-gg6qx6qf.ap-southeast-2.elasticbeanstalk.com";
+  // backendHost = "TodoApplication00-backend-env.eba-gg6qx6qf.ap-southeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
